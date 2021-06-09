c76b5373ae1cce9aa9c0615903754203115ec7fa
Noah Schlag <noahschlag12@gmail.com>
2021-06-09 13:32:27 -0500

    gotten to the point in the code where prompts are implemented, and the user is asked a series of questions

--- Assets/master/index.js
+++ Assets/master/index.js
@@ -216,3 +216,76 @@ async function updateEmployeeRole() {
             choices: employeeChoices
         }
     ]);
+
+    const roles = await db.findAllRoles();
+
+    const roleChoices = roles.map(({ id, title }) => ({
+        name: title,
+        value: id
+    }));
+
+    const { roleId } = await prompt([
+        {
+            type: "list",
+            name: "roleId",
+            message: "Which role do you want to assign the selected employee?",
+            choices: roleChoices
+        }
+    ]);
+
+    await db.updateEmployeeRole(employeeId, roleId);
+
+    console.log("Updated employee's role");
+
+    loadMainPrompts();
+}
+
+async function updateEmployeeManager() {
+    const employees = await db.findAllEmployees();
+
+    const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+    }));
+
+    const { employeeId } = await prompt([
+        {
+            type: "list",
+            name: "employeeId",
+            message: "Which employee's manager do you want to update?",
+            choices: employeeChoices
+        }
+    ]);
+
+    const managers = await db.findAllPossibleManagers(employeeId);
+
+    const managerChoices = managers.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+    }));
+
+    const { managerId } = await prompt([
+        {
+            type: "list",
+            name: "managerId",
+            message:
+                "Which employee do you want to set as manager for the selected employee?",
+            choices: managerChoices
+        }
+    ]);
+
+    await db.updateEmployeeManager(employeeId, managerId);
+
+    console.log("Updated employee's manager");
+
+    loadMainPrompts();
+}
+
+async function viewRoles() {
+    const roles = await db.findAllRoles();
+
+    console.log("\n");
+    console.table(roles);
+
+    loadMainPrompts();
+}
