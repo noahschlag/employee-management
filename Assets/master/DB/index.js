65cbf1d7046b58c823335b564215975fc5c30b11
Noah Schlag <noahschlag12@gmail.com>
2021-06-09 13:13:19 -0500

    added more roles and actions to the application in index.js, thankfully I have help doing this it is a God send

--- Assets/master/DB/index.js
+++ Assets/master/DB/index.js
@@ -21,3 +21,71 @@ class DB {
     createEmployee(employee) {
         return this.connection.query("INSERT INTO employee SET ?", employee);
     }
+    removeEmployee(employeeId) {
+        return this.connection.query(
+            "DELETE FROM employee WHERE id = ?",
+            employeeId
+        );
+    }
+
+    updateEmployeeRole(employeeId, roleId) {
+        return this.connection.query(
+            "UPDATE employee SET role_id = ? WHERE id = ?",
+            [roleId, employeeId]
+        );
+    }
+
+    updateEmployeeManager(employeeId, managerId) {
+        return this.connection.query(
+            "UPDATE employee SET manager_id = ? WHERE id = ?",
+            [managerId, employeeId]
+        );
+    }
+
+    findAllRoles() {
+        return this.connection.query(
+            "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    createRole(role) {
+        return this.connection.query("INSERT INTO role SET ?", role);
+    }
+
+    removeRole(roleId) {
+        return this.connection.query("DELETE FROM role WHERE id = ?", roleId);
+    }
+
+    findAllDepartments() {
+        return this.connection.query(
+            "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
+        );
+    }
+
+    createDepartment(department) {
+        return this.connection.query("INSERT INTO department SET ?", department);
+    }
+
+    removeDepartment(departmentId) {
+        return this.connection.query(
+            "DELETE FROM department WHERE id = ?",
+            departmentId
+        );
+    }
+
+    findAllEmployeesByDepartment(departmentId) {
+        return this.connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
+            departmentId
+        );
+    }
+
+    findAllEmployeesByManager(managerId) {
+        return this.connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+            managerId
+        );
+    }
+}
+
+module.exports = new DB(connection);
--- Assets/master/index.js
+++ Assets/master/index.js
@@ -49,3 +49,66 @@ async function loadMainPrompts() {
                     name: "Update Employee Manager",
                     value: "UPDATE_EMPLOYEE_MANAGER"
                 },
+                {
+                    name: "View All Roles",
+                    value: "VIEW_ROLES"
+                },
+                {
+                    name: "Add Role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "Remove Role",
+                    value: "REMOVE_ROLE"
+                },
+                {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
+                {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT"
+                },
+                {
+                    name: "Remove Department",
+                    value: "REMOVE_DEPARTMENT"
+                },
+                {
+                    name: "Quit",
+                    value: "QUIT"
+                }
+            ]
+        }
+    ]);
+
+    switch (choice) {
+        case "VIEW_EMPLOYEES":
+            return viewEmployees();
+        case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+            return viewEmployeesByDepartment();
+        case "VIEW_EMPLOYEES_BY_MANAGER":
+            return viewEmployeesByManager();
+        case "ADD_EMPLOYEE":
+            return addEmployee();
+        case "REMOVE_EMPLOYEE":
+            return removeEmployee();
+        case "UPDATE_EMPLOYEE_ROLE":
+            return updateEmployeeRole();
+        case "UPDATE_EMPLOYEE_MANAGER":
+            return updateEmployeeManager();
+        case "VIEW_DEPARTMENTS":
+            return viewDepartments();
+        case "ADD_DEPARTMENT":
+            return addDepartment();
+        case "REMOVE_DEPARTMENT":
+            return removeDepartment();
+        case "VIEW_ROLES":
+            return viewRoles();
+        case "ADD_ROLE":
+            return addRole();
+        case "REMOVE_ROLE":
+            return removeRole();
+        default:
+            return quit();
+    }
+}
